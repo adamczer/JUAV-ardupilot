878e33740a9d3ff9b96d6d1acc85f69e27069eef
Adam Czerniejewski <adamczer@buffalo.edu>
2020-06-08 21:04:43 -0400

    rename arducopter code so that it dosent refrence sitl

--- juav-hal/juav-hal-sitl/run-juav-copter.sh
+++ juav-hal/juav-hal-sitl/run-juav-copter.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-java -Xmx1G -Xms1G -cp /home/adamczer/code/juav2/juav-hal/juav-hal-sitl/target/juav-hal-sitl-*-jar-with-dependencies.jar -Djava.library.path=/home/adamczer/code/juav2/juav-native/juav-native-sitl/jni/lib/ ub.cse.juav.copter.HalSitl
+# ensure arducopter lib is in ldconfig
+#sudo ln -s /home/adamczer/code/juav2/juav-native/juav-native-ardupilot/jni/lib/libArduCopterSitl.so /usr/local/lib/
+# ls -la /usr/local/lib/libArduCopterSitl.so
+# sudo ldconfig
+# ./run-juav-copter.sh
+
+java -Xmx1G -Xms1G -cp /home/adamczer/code/juav2/juav-hal/juav-hal-sitl/target/juav-hal-sitl-*-jar-with-dependencies.jar -Djava.library.path=/home/adamczer/code/juav2/juav-native/juav-native-ardupilot/jni/lib/ ub.cse.juav.copter.HalSitl
