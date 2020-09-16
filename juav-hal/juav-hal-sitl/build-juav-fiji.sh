c68a76b7a0abbd6b8656a31a5dcb5c23fe7ea69b
Adam Czerniejewski <adamczer@buffalo.edu>
2020-09-16 14:16:42 -0400

    code to test with other build changes

--- juav-hal/juav-hal-sitl/build-juav-fiji.sh
+++ juav-hal/juav-hal-sitl/build-juav-fiji.sh
@@ -2,26 +2,16 @@
 rm -rf juav-fiji
 mkdir juav-fiji
 cd juav-fiji
-mkdir juav-jars
-cd juav-jars
-unzip ../../target/juav-hal-sitl-*-with-dependencies.jar
-rm -rf META-INF
-unzip $FIJI_HOME/lib/rtsj.jar
-rm -rf META-INF
-cd ..
-mkdir build
-find juav-jars -name \*.class -exec cp {} build/ \;
-mkdir libs
-cp ../../../juav-native/juav-native-ardupilot/jni/lib/lib*Sitl*.so ./libs/
+
 
 $FIJI_HOME/bin/fivmc \
---link-dir libs \
---link-dynamic ArduCopterSitl \
---link-dynamic JuavSitlJni \
--o JuavFiji ./build/*.class \
+--sys-libs "-lpthread -ldl -lm -lJuavSitlJni" \
+-o JuavFiji ../target/juav-hal-sitl-*-with-dependencies.jar $JUAV_SRC/lib/rtsj.jar \
 --64 \
 --main ub.cse.juav.copter.HalSitl
 
-cp libs/* .
-./JuavFiji fiji
-cd ..
+cp /usr/local/lib/*Sitl*.so .
+echo "AS ROOT run:" 
+echo "> cd juav-fiji"
+echo "> export ARDU_SRC=/path/to/ardupilot/checkout"
+echo "> ./JuavFiji fiji"
