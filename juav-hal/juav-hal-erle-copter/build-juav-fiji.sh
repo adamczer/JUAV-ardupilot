860c2857592fbbcf138ce92d3a09be518956efb4
Adam Czerniejewski <adamczer@buffalo.edu>
2020-12-10 22:40:56 -0500

    install gcc-4.8 gcc-4.8-multilib gcc-arm-linux-gnueabihf after building fiji add target with bin/add-target --cc /usr/bin/arm-linux-gnueabihf-gcc --posix --host arm-linux-gnueabihf --ptr-size 32 --xtra-cflags -Wno-implicit-fallthrough this will allow cross compiling to arm target with fiji
    modified erle script to build cross or on copter by checking arch

--- juav-hal/juav-hal-erle-copter/build-juav-fiji.sh
+++ juav-hal/juav-hal-erle-copter/build-juav-fiji.sh
@@ -4,11 +4,17 @@ mkdir juav-fiji
 cd juav-fiji
 
 cp ../../../juav-native/juav-native-ardupilot/jni/lib/lib*Erle*.so ./
-$FIJI_HOME/bin/fivmc -j4 \
+if arch | grep -q "arm"; then
+ $FIJI_HOME/bin/fivmc -j4 \
 --sys-libs "-lpthread -ldl -lm -lJuavErleCopterJni" \
 -o JuavFiji ../target/*-with-dependencies.jar $JUAV_SRC/lib/rtsj.jar \
 --main ub.cse.juav.copter.HalLinuxClass
-
+else
+ $FIJI_HOME/bin/fivmc --target arm -j8 \
+--sys-libs "-lpthread -ldl -lm -lJuavErleCopterJni" \
+-o JuavFiji ../target/*-with-dependencies.jar $JUAV_SRC/lib/rtsj.jar \
+--main ub.cse.juav.copter.HalLinuxClass
+fi
 echo "AS ROOT run:" 
 echo "> cd juav-fiji"
 echo "> export ARDU_SRC=/path/to/ardupilot/checkout"
