bf41f3a05a7623848e13cf2a28c4370364d63816
adamczer <adamczer@buffalo.edu>
2021-01-12 12:30:51 -0500

    changes to get mvm up and running for testing

--- juav-hal/juav-hal-sitl/build-juav-fiji-mvm.sh
+++ juav-hal/juav-hal-sitl/build-juav-fiji-mvm.sh
@@ -2,56 +2,38 @@
 rm -rf juav-fiji-mvm
 mkdir juav-fiji-mvm
 cd juav-fiji-mvm
-mkdir juav-jars
-cd juav-jars
-unzip ../../target/juav-hal-sitl-*-with-dependencies.jar
-rm -rf META-INF
-unzip $JUAV_SRC/lib/fivmcommon.jar
-rm -rf META-INF
-unzip $JUAV_SRC/lib/fivmr.jar
-rm -rf META-INF
-unzip $JUAV_SRC/lib/rtsj.jar
-rm -rf META-INF
-cd ..
-mkdir build
-find juav-jars -name \*.class -exec cp {} build/ \;
 
 $FIJI_HOME/bin/fivmc \
 --sys-libs "-lpthread -ldl -lm -lJuavSitlJni" \
--o JuavFiji ./build/*.class \
---gc CMR \
---payload \
---rt-library=RTSJ \
---main ub.cse.juav.copter.HalSitl
-
-${FIJI_HOME}/bin/fivmc \
+-o JuavFiji ../target/juav-hal-sitl-0.1-SNAPSHOT-jar-with-dependencies.jar \
 --32 \
---more-opt \
---c-opt SPEED \
 --g-def-max-mem 256M \
 --g-def-immortal-mem 128M \
 --payload \
 --rt-library=RTSJ \
---gc CMR \
+--rt-verbosity-limit 100 \
+--main ub.cse.juav.copter.HalSitl
+
+${FIJI_HOME}/bin/fivmc \
+--32 \
+--g-def-max-mem 256M \
+--g-def-immortal-mem 128M \
+--payload \
+--rt-library=RTSJ \
+--rt-verbosity-limit 100 \
 -o apps \
-../../../juav-payloads/target/classes/ub/cse/juav/payloads/simple/SimplePayload.class 
+../../../juav-payloads/target/juav-payloads-0.1-SNAPSHOT-jar-with-dependencies.jar \
+--main ub.cse.juav.payloads.simple.SimplePayload
 
 ${FIJI_HOME}/bin/fivmc \
 -o mvm \
 --sys-libs "-lpthread -ldl -lm -lJuavSitlJni" \
 --32 \
---g-def-max-mem 512M \
---g-def-immortal-mem 256M \
+--g-def-max-mem 1G \
+--g-def-immortal-mem 512M \
 --link-payload JuavFiji \
 --link-payload apps \
---gc CMR \
-../../../juav-payloads/target/classes/ub/cse/juav/payloads/vmconfig/VMConfig.class
-
-
-#--sys-libs "-lpthread -ldl -lm -lJuavSitlJni" \
-#--link-payload JuavFiji \
-#--link-payload apps \
-
-#cp libs/* .
-#./JuavFiji fiji
-#cd ..
+--rt-library=RTSJ \
+--rt-verbosity-limit 100 \
+../../../juav-payloads/target/juav-payloads-0.1-SNAPSHOT-jar-with-dependencies.jar \
+--main ub.cse.juav.payloads.vmconfig.VMConfig
